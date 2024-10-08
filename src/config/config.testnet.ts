545938b87038070e4a04b8dbdd48ebcc29a7daf4
constantintovisi <constantin.tovisi@elrond.com>
2024-10-08 11:12:35 +0300

    Initial commit

---
+++ src/config/config.testnet.ts
@@ -0,0 +1,11 @@
+import { EnvironmentsEnum } from 'types';
+
+export * from './sharedConfig';
+
+export const contractAddress =
+  'erd1qqqqqqqqqqqqqpgq8tq5rulzxzje29v8kzmcxx9pgx6kmevmep6qckwthl';
+export const API_URL = 'https://testnet-template-api.multiversx.com';
+export const sampleAuthenticatedDomains = [API_URL];
+export const environment = EnvironmentsEnum.testnet;
+export const metamaskSnapWalletAddress =
+  'https://testnet-snap-wallet.multiversx.com';
